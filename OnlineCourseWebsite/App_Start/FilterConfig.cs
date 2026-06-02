ada30690e55a94bbb2caeafb5451df07ebc9e8cf
Tie <nguyenngocanhthu2005.rg@gmail.com>
2026-06-02 18:43:32 +0700

    create MVC visual code

---
+++ OnlineCourseWebsite/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineCourseWebsite
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
