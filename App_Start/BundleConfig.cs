69405843079bf8dcafb116d6af133a738c203399
tiegyen <nguyenngocanhthu2005.rg@gmail.com>
2026-06-06 14:24:17 +0700

    put nav in layout.cshtml

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -23,8 +23,10 @@ namespace OnlineCourseWebsite
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+      "~/Content/bootstrap.css",
+      "~/Content/site.css",
+      "~/css/style.css"
+));
         }
     }
 }
