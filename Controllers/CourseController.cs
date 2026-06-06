66f5e18742cac81d5f9db0ba7d849ad199ab0546
tiegyen <nguyenngocanhthu2005.rg@gmail.com>
2026-06-06 18:06:44 +0700

    update pagedList_filterBoxCategory

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -4,18 +4,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using PagedList;
 namespace OnlineCourseWebsite.Controllers
 {
     public class CourseController : Controller
     {
         dbOnlineCourseDataContext db = new dbOnlineCourseDataContext();
         // GET: Course
-        public ActionResult Course()
+        public ActionResult Course(int? page, int? currentCategory)
         {
-            var allCourses = db.Courses.ToList();
+            int pageSize = 4;
+            int pageNumber = (page ?? 1); // nếu page bị null thì mặc định lấy page 1 
+
+            var coursesQuery = db.Courses.AsQueryable();
+
+            if (currentCategory.HasValue && currentCategory.Value > 0)
+            {
+                coursesQuery = coursesQuery.Where(c => c.CategoryID == currentCategory.Value);
+            }
+
+            var allCourses = coursesQuery.OrderBy(c=>c.CourseID).ToList();  
+
             ViewBag.Categories = db.Categories.ToList();
-            return View(allCourses);
+
+            ViewBag.CurrentCategory = currentCategory;
+
+            return View(allCourses.ToPagedList(pageNumber,pageSize));
         }
     }
 }
